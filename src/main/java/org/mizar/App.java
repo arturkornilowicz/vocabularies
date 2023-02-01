4490fcd65f526f3166f81f91f7113db5064ecf5c
Artur Kornilowicz <arturk@math.uwb.edu.pl>
2023-02-01 10:57:13 +0100

    Asking about a path to mml.vct file added

--- src/main/java/org/mizar/App.java
+++ src/main/java/org/mizar/App.java
@@ -6,6 +6,10 @@ public class App
 {
     public static void main( String[] args )
     {
+        if (args.length == 0) {
+            System.out.println("\nEnter path to mml.vct file!");
+            return;
+        }
         Vocabularies vocabularies = new Vocabularies(args[0]);
         vocabularies.loadMmlVct();
         System.out.println(vocabularies);
